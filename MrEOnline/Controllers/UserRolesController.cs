21a9cc15e02524b03b03d727424fa75072a6aa77
ZamoSithole <40822780+ZamoSithole@users.noreply.github.com>
2018-07-27 15:38:26 +0200

    Adding Roles to User

--- MrEOnline/Controllers/UserRolesController.cs
+++ MrEOnline/Controllers/UserRolesController.cs
@@ -1,10 +1,13 @@
-﻿using System.Data.Entity;
+﻿using System;
+using System.Data.Entity;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
+using MrE.Models;
+using MrE.Models.Abstractions;
 using MrE.Models.Entities;
 using MrE.Services.Abstractions;
 using MrEOnline.Models;
@@ -54,5 +57,41 @@ namespace MrEOnline.Controllers {
 
             return View(uRole);
         }
+        [HttpPost]
+        public virtual async Task<ActionResult> Create(UserRoleViewModel userRoleView) {
+
+            ViewBag.RoleSelectList = new SelectList(await RoleService.Roles.ToListAsync(), "Id", "Name");
+            var viewMessage = new ViewMessage();
+            if (!ModelState.IsValid) return View(userRoleView);
+            try {
+                var role = RoleService.Roles.SingleOrDefault(
+                        e => e.Id.CompareTo(userRoleView.Role.RoleId) == 0
+                    );
+                if (role == null) {
+                    viewMessage.Message = "Could not find role.";
+                    viewMessage.Type = ViewMessageType.Error;
+                }else {
+                    UserManager.AddToRole(userRoleView.UserId, role.Name);
+                    viewMessage.Message = "Successfully saved your changes.";
+                    viewMessage.Type = ViewMessageType.Success;
+
+                    return RedirectToAction("Edit", "Users", new {
+                        id = (userRoleView as IBaseEntity<int>).Id,
+                        message = viewMessage.Message,
+                        messageType = viewMessage.Type
+                    });
+                }
+                
+
+            } catch (Exception exception) {
+
+                viewMessage.Type = ViewMessageType.Success;
+                if (exception is ValidationException)
+                    viewMessage.Message = exception.ToString();
+                else
+                    viewMessage.Message = "There was an error processing your request, please try again later!!";
+            }
+            return View(userRoleView);
+        }        
     }
 }
